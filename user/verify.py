ca72dd18f86136826078c4002f0a5625056bb12c
imviz <vishnuedappatt@gmail.com>
2022-06-26 23:45:01 +0530

    updated

--- user/verify.py
+++ user/verify.py
@@ -5,7 +5,7 @@ from twilio.base.exceptions import TwilioRestException
 from django.conf import settings
 
 client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-verify = client.verify.services(os.environ['TWILIO_VERIFY_SERVICE_SID'])
+verify = client.verify.services(settings.TWILIO_VERIFY_SERVICE_SID)
 
 
 def send(phone_number):
